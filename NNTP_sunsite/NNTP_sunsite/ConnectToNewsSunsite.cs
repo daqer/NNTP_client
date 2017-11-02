acf9330cc2d5c6e95a4645e476594bffe299af4f
daqer <31531493+daqer@users.noreply.github.com>
2017-11-02 09:44:25 +0100

    Add files via upload

---
+++ NNTP_sunsite/NNTP_sunsite/ConnectToNewsSunsite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNTP_sunsite
+{
+    class ConnectToNewsSunsite
+    {
+    }
+}
